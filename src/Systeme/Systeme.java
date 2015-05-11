2815c3ebdd9232e1660b29a04d5819a11815b2ca
RAFFESTIN <romain.raffestin@etu.univ-orleans.fr>
2015-05-11 17:07:14 +0200

    finalisation fct Succ & Pred

--- src/Systeme/Systeme.java
+++ src/Systeme/Systeme.java
@@ -2,6 +2,7 @@ package Systeme;
 
 import Systeme.SystemeElem;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 /**
@@ -38,27 +39,24 @@ public class Systeme {
         this.se = s;
     }
 
-    public ArrayList succ (Integer i){ // ordre à revoir
+    public ArrayList succ (Object i){
         int x,lt;
         ArrayList l = new ArrayList();
-        //ArrayList lt = new ArrayList();
         if(i.getClass().equals(compareType(type.INT))){
             ArrayList<Integer> currentList;
-            currentList = trans.get(i);
+            currentList = trans.get(i);// à corriger
             for (x=1;x<currentList.size();x++) {
                 if(currentList.get(x) != -1){
                     l.add(currentList.get(x));
                 }
             }
         }
-
         if(i.getClass().equals(compareType(type.STR))){
             //à revoir si bo parcour de tableau
             boolean y=false;
             int j,z;
             if (se.nomEtiq.contains(i) && se.nomProp.contains(i)){//1
                 System.out.println("Problème de confusion lors des initialisations des propriétées et étiquettes");
-
             }else if (se.nomEtiq.contains(i)){//3
                 x = 0;
                 while(y==false){
@@ -67,7 +65,7 @@ public class Systeme {
                     }else x++;
                 }
                 for (j=1;j<transEtiq.size();j++){
-                    ArrayList<ArrayList<Integer>> currentList = transEtiq.get(j);
+                    ArrayList<ArrayList<Integer>> currentList = transEtiq.get(j);//attention au type du table potentiellement un string
                     ArrayList<Integer> currentElem = currentList.get(x);
                     if (currentElem.get(0) != -1){
                         for(z=0;z<currentElem.size();z++){
@@ -86,127 +84,135 @@ public class Systeme {
                     }else x++;
                 }
                 ArrayList< Boolean> X = prop.get(x);
-                for(z=0;z<X.size();z++){
-                    if (X.get(i)==true){
-                        tmp.add(i);
+                for(z=0;z<X.size();z++){//warning sur z, i avant
+                    if (X.get(z)==true){
+                        tmp.add(z);
                     }
                 }
                 for (j=0;j<trans.size();j++){
                     ArrayList<Integer> currentList = trans.get(j);
-                    while(currentList.get(j) != -1){
+                    while(currentList.get(j) != -1){//on doit retirer la première ligne du tableau
                         if (!l.contains(currentList.get(j))){
                             l.add(currentList.get(j));
                         }
                         j++;
                     }
                 }
-                Collections.sort(l);
-
             }else {//2
                 System.out.println("Aucune solution");
-
             }
         }
-
-
         Collections.sort(l);
         return l;
     }
 
-
-
-
-    //(obj1.getClass().equals(obj2.getClass())
-
-    public ArrayList<Integer> succ (ArrayList<Integer> p){
+    public ArrayList<Integer> succ (ArrayList p){
         int x;
-        ArrayList l;
         ArrayList l2 = new ArrayList();
-        for (x=1;x<p.size();x++) {
-            l = succ(p.get(x));
-            l2.addAll(l);
+        for (x=0 ; x<p.size();x++) {
+            l2.add(succ(p.get(x)));
         }
         Collections.sort(l2);
         return l2;
     }
 
-    public ArrayList<Integer> pred (Integer i) {
+    public ArrayList<Integer> predFct (Object i,type t){
         int x,y;
         ArrayList l = new ArrayList();
-        ArrayList<Integer> currentList;
-        for (x=1;x<trans.size();x++) {
-            currentList = trans.get(x);
+        ArrayList currentList;
+        for (x=1; x<selectType(t).size() ;x++) {
+            currentList = (ArrayList) selectType(t).get(x);
             for (y = 1; y < currentList.size(); y++) {
-                if (i == currentList.get(y)) {
+                if (i == currentList.get(y) && !l.contains(currentList.get(y))) {
                     l.add(x);
                 }
             }
         }
-        //Collections.sort(l);
         return l;
     }
 
-    public ArrayList<Integer> pred (ArrayList<Integer> p) {
+    public ArrayList pred (Object i) {
+        ArrayList l = new ArrayList();
+        if(i.getClass().equals(compareType(type.INT))){
+            l= predFct(i,type.INT);
+        }
+        if(i.getClass().equals(compareType(type.STR))){
+            if (se.nomEtiq.contains(i) && se.nomProp.contains(i)){
+                System.out.println("Problème de confusion lors des initialisations des propriétées et étiquettes");
+                l = null;
+            }else if (se.nomEtiq.contains(i)){
+                l= predFct(i,type.STR);//attrention à vérifier avec la construction du tableau (ici string) surement à changer.
+            }else if (se.nomProp.contains(i)){
+                int x,y,z;
+                ArrayList S = new ArrayList();
+                ArrayList T;
+
+                for(x=0;x<se.nomProp.size();x++){
+                    if (se.nomProp.get(x)==i){
+                        break;
+                    }
+                }
+                for (y=0; y<prop.size() ; y++){
+                    T=prop.get(y);
+                    if(T.get(x)==i){
+                        S.add(y);
+                    }
+                }
+                for (z=0;z<S.size();z++){
+                    l.addAll(predFct(S.get(z),type.INT));
+                }
+            }else {//2
+                System.out.println("Aucune solution");
+                l = null;
+            }
+        }
+        Collections.sort(l);
+        return l;
+    }
+
+    public ArrayList<Integer> pred (ArrayList p){
         int x;
-        ArrayList l;
         ArrayList l2 = new ArrayList();
-        for (x=1;x<p.size();x++) {
-            l = pred(p.get(x));
-            l2.addAll(l);
+        for (x=0 ; x<p.size();x++) {
+            l2.add(pred(p.get(x)));
         }
         Collections.sort(l2);
         return l2;
     }
 
+    public Object compareType(type t) {
+        Integer i = 0;
+        String s = "s";
 
+        switch (type) {
+            case INT:
+                return i.getClass();
 
-//iterate on the general list
- /*   for(int i = 0 ; i < collection.size() ; i++) {
-        ArrayList<String> currentList = collection.get(i);
-        //now iterate on the current list
-        for (int j = 0; j < currentList.size(); j++) {
-            String s = currentList.get(1);
+            case STR:
+                return s.getClass();
+
+            default:
+                return null;
         }
     }
 
-// initialise the collection
-    collection = new ArrayList<ArrayList<String>>();
-// iterate
-    for (ArrayList<String> innerList : collection) {
-        for (String string : innerList) {
-            // do stuff with string
+    public ArrayList selectType(type t) {
+
+        switch (t) {
+            case INT:
+                return trans;
+
+            case STR:
+                return transEtiq;
+
+            default:
+                return null;
         }
-    }*/
-
-public Object compareType(type t) {
-    Integer i = 0;
-    String s = "s";
-    Boolean b = true;
-    switch (type) {
-        case INT:
-            return i.getClass();
-
-        case STR:
-            return s.getClass();
-
-        case BOO:
-            return b.getClass();
-
-
-        default:
-            return null;
     }
-
-}
 
     public enum type {
         INT,
         STR,
         BOO;
     }
-
-
 }
-
-
-
